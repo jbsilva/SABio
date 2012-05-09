5ad67b55294993226f5a7a534cdf51631c8dc42b
unknown <Andre@Andre-PC.(none)>
2012-05-09 02:17:57 -0300

    AtendenteJDBCDAO

--- SABio/src/dao/impl/jdbc/AtendenteJDBCDAO.java
+++ SABio/src/dao/impl/jdbc/AtendenteJDBCDAO.java
@@ -1,13 +1,113 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao.impl.jdbc;
 
-/**
- *
- * @author Andre
- */
-public class AtendenteJDBCDAO {
+import dao.DAOException;
+import dao.DAOFactory;
+import dao.spec.ITreinoDAO;
+import dao.spec.IUsuarioDAO;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Properties;
+import vo.AtendenteVO;
+import vo.ObjectVO;
+
+
+public class AtendenteJDBCDAO extends GenericJDBCDAO implements ITreinoDAO {
+
+	public AtendenteJDBCDAO(Properties properties) throws DAOException {
+		super(properties);
+	}
+
+    @Override
+	public void insert(ObjectVO vo) throws DAOException {
+		String sql = "UPDATE " + this.getTableName()
+				+ " (LOGIN, NOME, CPF, RG, ENDERECO, NUMERO_CARTEIRA_TRABALHO, DATA_CONTRATACAO) = (?,?,?,?,?,?,?)";
+		try {
+                        AtendenteVO atendente = (AtendenteVO) vo;
+			PreparedStatement stmt = this.getConnection().prepareStatement(sql);
+			
+			stmt.setString(1, atendente.getUsuario());
+			stmt.setString(2, atendente.getNome());
+			stmt.setString(3, atendente.getCPF());
+                        stmt.setString(4, atendente.getRG());
+                        stmt.setString(5, atendente.getEndereco());
+                        stmt.setString(6, atendente.getNumeroCarteiraTrabalho());
+                        Date dt = new Date(atendente.getDataContratacao().getTime().getTime());
+			stmt.setDate(7, dt);
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+
+    @Override
+	public void update(ObjectVO vo) throws DAOException {
+		String sql = "UPDATE " + this.getTableName()
+				+ " (LOGIN, NOME, CPF, RG, ENDERECO, NUMERO_CARTEIRA_TRABALHO, DATA_CONTRATACAO) = (?,?,?,?,?,?,?)";
+		try {
+                        AtendenteVO atendente = (AtendenteVO) vo;
+			PreparedStatement stmt = this.getConnection().prepareStatement(sql);
+			
+			stmt.setString(1, atendente.getUsuario());
+			stmt.setString(2, atendente.getNome());
+			stmt.setString(3, atendente.getCPF());
+                        stmt.setString(4, atendente.getRG());
+                        stmt.setString(5, atendente.getEndereco());
+                        stmt.setString(6, atendente.getNumeroCarteiraTrabalho());
+                        Date dt = new Date(atendente.getDataContratacao().getTime().getTime());
+			stmt.setDate(7, dt);
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
     
+	public void SelectByUsuario(ObjectVO vo) throws DAOException {
+		String sql = "UPDATE " + this.getTableName()
+				+ " (LOGIN, NOME, CPF, RG, ENDERECO, NUMERO_CARTEIRA_TRABALHO, DATA_CONTRATACAO) = (?,?,?,?,?,?,?)";
+		try {
+                        AtendenteVO atendente = (AtendenteVO) vo;
+			PreparedStatement stmt = this.getConnection().prepareStatement(sql);
+			
+			stmt.setString(1, atendente.getUsuario());
+			stmt.setString(2, atendente.getNome());
+			stmt.setString(3, atendente.getCPF());
+                        stmt.setString(4, atendente.getRG());
+                        stmt.setString(5, atendente.getEndereco());
+                        stmt.setString(6, atendente.getNumeroCarteiraTrabalho());
+                        Date dt = new Date(atendente.getDataContratacao().getTime().getTime());
+			stmt.setDate(7, dt);
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+
+    @Override
+	public String getTableName() {
+		return "ATENDENTE";
+	}
+
+    @Override
+	protected ObjectVO createVO(ResultSet rs) throws DAOException {
+		try {
+			String login = rs.getString("LOGIN");
+			String nome = rs.getString("NOME");
+			String cpf = rs.getString("CPF");
+                        String rg = rs.getString("RG");
+			String endereco = rs.getString("ENDERECO");
+                        String numero_carteira_trabalho = rs.getString("NUMERO_CARTEIRA_TRABALHO");
+                        Date dt = rs.getDate("DATA_CONTRATACAO");
+                        
+                        Calendar cal = new GregorianCalendar();
+			cal.setTime(dt);
+                        
+			return new AtendenteVO(login, nome, cpf, rg, endereco, numero_carteira_trabalho, cal);
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
 }
